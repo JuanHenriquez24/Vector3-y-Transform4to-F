908b8f82d9bda2016c6b628280ae9ca4dfc35615
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-05-17 10:41:08 -0300

    Did controller, movement, and etc.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Vector3 movementspeed;
+
+    // Use this for initialization
+    void Start() {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position += movementspeed;
+        //transform.eulerAngles += movementspeed;
+        if (transform.position.x < 10) {
+            transform.position += movementspeed;
+        }
+    }
+}
---
+++ Assets/Vector3Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Script : MonoBehaviour {
+
+    public Vector3 miVector;
+    public GameObject myobject;
+	// Use this for initialization
+	void Start () {
+        myobject.transform.position = miVector;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
